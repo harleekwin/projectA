b6eb0a4c5f54ae13db801fd6a30cbd1485b22da4
Mary White <mwhite2503@gmail.com>
2017-04-27 17:15:38 -0700

    step1

---
+++ ngApp/controllers/controllers.ts
@@ -0,0 +1,34 @@
+namespace myapp.Controllers {
+
+    const apiUrl = '/api/cars/search';
+
+    export class HomeController {
+        public message = 'Hello from the home page!';
+        public cars;
+        public search;
+
+        public fetch() {
+          this.$http.get(apiUrl + this.search).then((res) => {
+            console.log(res);
+            this.cars = res.data;
+          })
+        }
+
+        constructor(private $http: ng.IHttpService) {
+            this.$http.get('/api/cars')
+                .then((response) => {
+                    this.cars = response.data;
+                })
+                .catch((response) => {
+                    console.error('Could not retrieve cars.');
+                });
+        }
+
+    }
+
+
+    export class AboutController {
+        public message = 'Welcome to the about page!';
+    }
+
+}
